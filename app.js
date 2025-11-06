400b62d1a6deb86aac7dc7f03892b17b48e92012
thaventhiran Ramsan <ramsandota@gmail.com>
2025-11-07 05:28:19 +0530

    npm dotenv

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 // Basic server setup
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/book-link");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log(" MongoDB Connected!");
   } catch (error) {
     console.error(" Error connecting to MongoDB:", error);
   }
 };
-
 connectDB();
 import readerRouter from "./routes/readerRouter.js";
 import bookRouter from "./routes/bookRouter.js";
 import reviewRouter from "./routes/reviewRouter.js";
 import transactionRouter from "./routes/transactionRouter.js";
-
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
 
 app.use("/api/readers", readerRouter);
 app.use("/api/books", bookRouter);
